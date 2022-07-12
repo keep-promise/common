2dceda1bd43edb756c4ae825f6848048458b6ec0
keep-promise <64321089+keep-promise@users.noreply.github.com>
2022-07-12 19:50:25 +0800

    Create index.ts
    精确计算

---
+++ calc-precise/index.ts
@@ -0,0 +1,59 @@
+/**
+精确计算
+1.先转整数
+2.整数运算
+3.化为小数
+*/
+
+
+
+/**
+ * @description: 小数点后的长度
+ * @param {number} num
+ * @return {*}
+ */
+function decimalDigits(num: number | string): number {
+
+}
+
+/**
+ * @description: 精确的加法 0.1+0.2：先转整数相加
+ * @param {number} a
+ * @param {number} b
+ * @return {*}
+ */
+function add(a: number | string, b: number | string) {
+
+}
+
+console.log('js prototype add', 0.1 + 0.2);
+console.log('js add calc', add(0.1, 0.2));
+
+/**
+ * @description: 精确的乘法
+ * @param {number} a
+ * @param {number} b
+ * @return {*}
+ */
+function multiply(a: number | string, b: number | string) {
+
+}
+
+/**
+ * @description: 精确的除法
+ * @param {number} a
+ * @param {number} b
+ * @return {*}
+ */
+function divide(a: number | string, b: number | string) {
+
+}
+
+/**
+ * @description: 小数转百分数
+ * @param {number} a
+ * @return {*}
+ */
+function decimalToPercent(a: number) {
+
+}
